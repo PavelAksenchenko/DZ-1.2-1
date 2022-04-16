e8f612d8b4c9fa7d717809e8b090b3c927589abb
Pavel Aksenshenko <angelracing@yangex.ru>
2022-04-17 01:15:39 +0300

    Add commit 2

---
+++ src/test/java/ru/netology/rest/MobileBankApiTestV2.java
@@ -0,0 +1,30 @@
+package ru.netology.rest;
+
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+class MobileBankApiTestV2 {
+    @Test
+    void shouldReturnDemoAccounts() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .statusCode(200)
+                // .header("Content-Type", "application/json; charset=UTF-8")
+                // специализированные проверки - лучше
+                .contentType(ContentType.JSON)
+                .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
+                .body("[1].currency", equalTo("USD"));
+        ;
+    }
+}
